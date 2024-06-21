f69e5fc03376085d4a14e5c4a647a033d84f0133
SaltedEggCalamari <149132331+SaltedEggCalamari@users.noreply.github.com>
2024-06-21 18:13:33 +0800

    Update headers.cpp

--- projectTest/headers.cpp
+++ projectTest/headers.cpp
@@ -5,41 +5,31 @@
 /* Library inclusions */
 #include <string>
 #include <iostream>
-#include <chrono>
-#include <ctime>
+#include <fstream>
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <map>
 
 
-/* Passenger functions */
-void Passenger::get_info(){
-    cout << "id: " << passenger_id
-        << "\nDestination: " << destination
-        << "\nTime of arrival: " << time_of_arrival
-        << "\nAssigned?: " << assigned;        
+/* Friend functions */
+bool operator==(const Shuttle& lhs, const Passenger& rhs){
+    if(lhs.destination == rhs.destination && lhs.arrival_time == rhs.arrival_time) {return true;}
+    else {return false;}
 }
 
-void Passenger::set_assigned(bool input){
-    if(input == true){assigned = true;}
-    else {assigned = false;}
+bool operator<(const Shuttle& lhs, const Passenger& rhs){
+    if(lhs.destination < rhs.destination && lhs.arrival_time == rhs.arrival_time) {return true;}
+    else {return false;}
+}
+
+bool operator>(const Shuttle& lhs, const Passenger& rhs){
+    if(lhs.destination > rhs.destination && lhs.arrival_time == rhs.arrival_time) {return true;}
+    else {return false;}    
 }
 
 
-/* Shuttle functions */
-void Shuttle::get_info(){
-    cout << "id: " << shuttle_id
-        << "\nCharging point: " << charging_point
-        << "\nTime of arrival: " << time_of_arrival
-        << "\nAssigned?: " << assigned;        
-}
-
-void Shuttle::set_assigned(bool input){
-    if(input == true) {assigned = true;}
-    else {assigned = false;}
-}
-
-/*convert time string to format time*/
+/* Time formatting function */
 int timeToMinutes(const std::string& time) {
     std::istringstream ss(time);
     int hours, minutes;
@@ -52,81 +42,70 @@ int timeToMinutes(const std::string& time) {
     return hours * 60 + minutes;
 }
 
-/* Friend functions */
-bool operator==(const Shuttle& lhs, const Passenger& rhs){
-    lhs_placetime = lhs.charging_point + lhs.time_of_arrival
-    rhs_placetime = rhs.destination + rhs.time_of_arrival
-    if(lhs_placetime == rhs_place) {return true;}
-    else {return false;}
-}
-
-bool operator<(const Shuttle& lhs, const Passenger& rhs){
-    lhs_placetime = lhs.charging_point + lhs.time_of_arrival
-    rhs_placetime = rhs.destination + rhs.time_of_arrival
-    if(lhs_placetime < rhs_place) {return true;}
-    else {return false;}
-}
-
-bool operator>(const Shuttle& lhs, const Passenger& rhs){
-    lhs_placetime = lhs.charging_point + lhs.time_of_arrival
-    rhs_placetime = rhs.destination + rhs.time_of_arrival
-    if(lhs_placetime > rhs_place) {return true;}
-    else {return false;}    
-}
-
-
-/* ScheduleEntry functions */
-void ScheduleEntry::set_pair(const Shuttle& shuttle, const Passenger& passenger){
-
-}
-
-void ScheduleEntry::edit_pair(const string& label, const Shuttle& updatedShuttle){
-
-}
-
-void ScheduleEntry::delete_pair(const string& label){
-
-}
-
-void ScheduleEntry::get_pair(){
-
-}
-
 
 /* RoutePlanner functions */
-void RoutePlanner::import_schedule(const string& shuttleFile, const string& passengerFile){
+vector<Passenger> RoutePlanner::import_passenger(const string& passengerFile){
+    vector<Passenger> passengers;
 
+    ifstream file(passengerFile);
+    string line;
+    while (getline(file, line)) {
+        istringstream iss(line);
+        string id, destination, time;
+        getline(iss, id, ',');
+        getline(iss, destination, ',');
+        getline(iss, time, ',');
+        passengers.emplace_back(Passenger(id, destination, time));
+    }
+
+    return passengers;
+}
+
+vector<Shuttle> RoutePlanner::import_shuttle(const string& shuttleFile){
+    vector<Shuttle> shuttles;
+
+    ifstream file(shuttleFile);
+    string line;
+    while (getline(file, line)) {
+        istringstream iss(line);
+        string id, destination, time;
+        getline(iss, id, ',');
+        getline(iss, destination, ',');
+        getline(iss, time, ',');
+        shuttles.emplace_back(Shuttle(id, destination, time));
+    }
+
+    return shuttles;
 }
 
 vector<ScheduleEntry> RoutePlanner::compute_schedule(vector<Shuttle> shuttles, vector<Passenger> passengers){
     
     /* Sorting algorithm */
     std::sort(shuttles.begin(), shuttles.end(), [](const Shuttle& a, const Shuttle& b) {
-        if (a.get_charging_point() == b.get_charging_point()) {                                     // Check if charging points are the same
-            return timeToMinutes(a.get_time_of_arrival()) < timeToMinutes(b.get_time_of_arrival()); // Compare by time of arrival if same
+        if (a.get_destination() == b.get_destination()) {                                     // Check if charging points are the same
+            return timeToMinutes(a.get_arrival_time()) < timeToMinutes(b.get_arrival_time()); // Compare by time of arrival if same
         }
-        return a.get_charging_point() < b.get_charging_point();                                     // Otherwise, compare by charging point
+        return a.get_destination() < b.get_destination();                                     // Otherwise, compare by charging point
     });
 
     std::sort(passengers.begin(), passengers.end(), [](const Passenger& a, const Passenger& b) {    
         if (a.get_destination() == b.get_destination()) {                                           // Check if destinations are the same
-            return timeToMinutes(a.get_time_of_arrival()) < timeToMinutes(b.get_time_of_arrival()); // If so, compare by time of arrival
+            return timeToMinutes(a.get_arrival_time()) < timeToMinutes(b.get_arrival_time()); // If so, compare by time of arrival
         }
         return a.get_destination() < b.get_destination();                                           // Otherwise, compare by destination
     });
     
-
     /* Matching algorithm */
     size_t index_s = 0, index_p = 0;                                                // Initialise pointers for vectors
     int pair_id = 1;                                                                // Initialise id for ScheduleEntry
 
-    vector<ScheduleEntry> returnVector;                                             // Instantiate vector to compile pairs
+    vector<ScheduleEntry> schedule_vector;                                             // Instantiate vector to compile pairs
 
     while (index_s < shuttles.size() && index_p < passengers.size())                // Loop ends when either index exceeds vector size
     {
         if(shuttles[index_s] == passengers[index_p]){                               // Refer to friend operator overload function
-            returnVector.emplace_back(ScheduleEntry(pair_id));                      // Instantiates new entry into back of vector
-            returnVector.back().set_pair(shuttles[index_s], passengers[index_p]);   // Set objects as pair in ScheduleEntry
+            schedule_vector.emplace_back(ScheduleEntry(pair_id));                      // Instantiates new entry into back of vector
+            schedule_vector.back().set_pair(shuttles[index_s], passengers[index_p]);   // Set objects as pair in ScheduleEntry
             pair_id++;                                                              // Increments id for next pair
 
             shuttles[index_s].set_assigned(true);                                   // Updates shuttle and passenger to reflect new assigned status
@@ -140,21 +119,144 @@ vector<ScheduleEntry> RoutePlanner::compute_schedule(vector<Shuttle> shuttles, v
         else if(shuttles[index_s] < passengers[index_p]) {index_s++;}               // by incrementing smaller index
     }
 
-    RoutePlanner::schedule_entries = returnVector;                                  // Updates RoutePlanner's data with vector of schedule entries
-
-    return returnVector;                                                            // Returns vector of schedule entries
+    return schedule_vector;                                                         // Returns vector of schedule entries
 }
 
-void RoutePlanner::get_schedule(){
+void RoutePlanner::show_all(vector<ScheduleEntry> schedule, vector<Passenger> u_passengers, vector<Shuttle> u_shuttles){
+    int size_schedule=schedule.size(), size_passengers=u_passengers.size(), size_shuttles=u_shuttles.size();
+    
+    size_t index=0;
+    cout << "\nSCHEDULE" << endl;
+    cout << "Number of entries: " << size_schedule << endl;
+    while(index < size_schedule){
+        Shuttle s = schedule[index].get_shuttle();
+        Passenger p = schedule[index].get_passenger();
+
+        cout << "ID: " << schedule[index].get_entry_id() << " --> " << s.get_shuttle_id() << " and " << p.get_passenger_id();
+    }
+
+    index = 0;
+    cout << "\n\nUNASSIGNED PASSENGERS" << endl;
+    cout << "Number of entries: " << size_passengers << endl;
+    while(index < size_shuttles){
+        cout << "ID: " <<  u_passengers[index].get_passenger_id()
+        << "\nGoing to " << u_passengers[index].get_destination() << " at " << u_passengers[index].get_arrival_time();
+    }
+
+    index = 0;
+    cout << "\n\nUNASSIGNED SHUTTLES" << endl;
+    cout << "Number of entries: " << size_shuttles << endl;
+    while(index < size_passengers){
+        cout << "ID: " <<  u_shuttles[index].get_shuttle_id()
+        << "\nGoing to " << u_shuttles[index].get_destination() << " at " << u_shuttles[index].get_arrival_time();
+    }
 
 }
 
-void RoutePlanner::export_schedule(const string& outputFile){
+void RoutePlanner::export_schedule(const string& outputFile, vector<ScheduleEntry> entries){
+    std::ofstream file(outputFile);
 
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << outputFile << " for writing." << std::endl;
+        return;
+    }
+
+    try {
+        size_t index = 0;
+
+        while(index < entries.size()){
+            Shuttle s = entries[index].get_shuttle();
+            Passenger p = entries[index].get_passenger();
+
+            file << "Schedule ID    : " << entries[index].get_entry_id()
+                << "\nShuttle ID    : " << s.get_shuttle_id()
+                << "\nPassenger ID  : " << p.get_passenger_id()
+                << "\nDestination   : " << s.get_destination()
+                << "\nArrival Time  : " << s.get_arrival_time();            
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Error: Exception occurred while writing to file: " << e.what() << std::endl;
+    }
 }
 
+void RoutePlanner::assess_passengers(vector<Passenger> passengers){
+    map<string, int> destination_map;
+    map<string, int> time_map;
 
+    for(int i=0; i < passengers.size(); i++){
+        string d = passengers[i].get_destination();
+        string t = passengers[i].get_arrival_time();
+        
+        auto it_d = destination_map.find(d);
+        if (it_d != destination_map.end()){
+            destination_map[d] += 1;
+        } else{
+            destination_map.insert({d, 1});
+        }
 
+        auto it_t = time_map.find(t);
+        if (it_t != time_map.end()){
+            time_map[t] += 1;
+        } else{
+            time_map.insert({t, 1});
+        }
+    }
+
+    string d_first, d_second, d_third;
+    int x=0, y=0, z=0;
+    for(const auto& pair: destination_map){
+        if(pair.second > x){
+            d_second = d_first;
+            y = x;
+
+            d_first = pair.first;
+            x = pair.second;
+
+        } else if(pair.second > y){
+            d_third = d_second;
+            z = y;
+
+            d_second = pair.first;
+            y = pair.second;
+            
+        } else if(pair.second > z){
+            d_third = pair.first;
+            z = pair.second;
+        }
+    }
+
+    string t_first, t_second, t_third;
+    x=0, y=0, z=0;
+    for(const auto& pair: time_map){
+        if(pair.second > x){
+            t_second = t_first;
+            y = x;
+
+            t_first = pair.first;
+            x = pair.second;
+
+        } else if(pair.second > y){
+            t_third = t_second;
+            z = y;
+
+            t_second = pair.first;
+            y = pair.second;
+            
+        } else if(pair.second > z){
+            t_third = pair.first;
+            z = pair.second;
+        }
+    }
+
+    cout << "Top 3 destinations:" << endl;
+    cout << "1. " << d_first
+        << "\n2. " << d_second
+        << "\n3. " << d_third << endl;
+    cout << "Top 3 timings:" << endl;
+    cout << "1. " << t_first
+        << "\n2. " << t_second
+        << "\n3. " << t_third << endl;
+}
 
 
 
